b1244536ae5f47101a1ba1d1a1fb0091a1cf4271
Crusader5033 <prochazka6@spsejecna.cz>
2023-06-04 11:19:35 +0200

    first commit

---
+++ src/components/footer.svelte
@@ -0,0 +1,27 @@
+<footer class=" text-light py-4" style="background-color: #808000;">
+    <div class="container text-center">
+      <p>© 2023 Your Name</p>
+      <ul class="list-inline mb-0">
+        <li class="list-inline-item">
+          <a href="#" class="social-link">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="social-link">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="social-link">
+            <i class="fa fa-instagram"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="social-link">
+            <i class="fa fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
